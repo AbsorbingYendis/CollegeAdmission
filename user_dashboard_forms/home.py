998f81eb9fc2ade15e1c202d3cece1ac927ccb65
Lawrence Afable <lawrencearellanoafable@gmail.com>
2024-07-06 08:28:30 +0800

    modify the admin_home_dashboard

--- user_dashboard_forms/home.py
+++ user_dashboard_forms/home.py
@@ -1,15 +1,12 @@
 import tkinter as tk
-from tkinter import *
 import ttkbootstrap as tb
 from ttkbootstrap.constants import *
-import sqlite3
-from tkinter import messagebox
 
 def home(content_frame):
-
+  
   # remove all widgets in content_frame
   for widget in content_frame.winfo_children():
         widget.destroy()
 
-  label = tb.Label(content_frame, text="This is home form", bootstyle="success")
+  label = tb.Label(content_frame, text="This is home frame")
   label.pack()
